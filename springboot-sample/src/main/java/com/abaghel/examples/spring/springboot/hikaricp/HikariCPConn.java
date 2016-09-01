548710f43254627945b4f3766610a7d1c1247b6d
Amit Baghel <baghel_amit@yahoo.com>
2016-09-01 15:06:46 +0530

    update

--- springboot-sample/src/main/java/com/abaghel/examples/spring/springboot/hikaricp/HikariCPConn.java
+++ springboot-sample/src/main/java/com/abaghel/examples/spring/springboot/hikaricp/HikariCPConn.java
@@ -2,23 +2,47 @@ package com.abaghel.examples.spring.springboot.hikaricp;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 /**
  * 
  * @author abaghel
  *
  */
 @Component
+@ConfigurationProperties
 public class HikariCPConn {
 	
-	@Autowired
-	DataSource dataSource;
+	@Value("${spring.datasource.driverClassName}")
+	private String driverClassName;
+
+	@Value("${spring.datasource.url}")
+	private String url;
+
+	@Value("${spring.datasource.username}")
+	private String user;
+
+	@Value("${spring.datasource.password}")
+	private String password;
+
+	@Bean
+	public DataSource getDataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setUsername(user);
+		hikariConfig.setPassword(password);
+		hikariConfig.setDriverClassName(driverClassName);
+		hikariConfig.setJdbcUrl(url);
+		return new HikariDataSource(hikariConfig);
+	}
 
 	@Bean
 	public JdbcTemplate getJdbcTemplate() {
-		return new JdbcTemplate(dataSource);
+		return new JdbcTemplate(getDataSource());
 	}
 }
